582346472bf1e72ab036f1f5b201e960289f1431
jorge <jsgonzales10@up.edu.ph>
2026-02-19 15:10:08 +0800

    succesful send

--- src/routes/api/locations.ts
+++ src/routes/api/locations.ts
@@ -8,15 +8,22 @@ const router = Router();
 
 // POST /api/locations
 router.post("/", async (req: Request, res: Response) => {
-  const { lat, lng, sticker_type } = req.body;
+  const { stickers } = req.body;
 
-  if (typeof lat !== "number" || typeof lng !== "number") {
-    return res.status(400).json({ error: "lat and lng must be numbers" });
-  }
+  // if (typeof lat !== "number" || typeof lng !== "number") {
+  //   return res.status(400).json({ error: "lat and lng must be numbers" });
+  // }
+
+  //take lat, lng, and sticker_type
+  const sticker_rows = stickers.map((s: any) => ({
+    lat: Number(s.lat),
+    lng: Number(s.lng),
+    sticker_type: String(s.sticker_type),
+  }));
 
   const { data, error } = await db
     .from("locations")
-    .insert([{ lat, lng }])
+    .insert( sticker_rows )
     .select();
 
   if (error) {
