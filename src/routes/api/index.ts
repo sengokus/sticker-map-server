1ba7a0240f1731d4439e4c18ece21f5294d78991
jorge <jsgonzales10@up.edu.ph>
2026-02-25 21:47:56 +0800

    route

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -2,12 +2,14 @@ import dotenv from "dotenv";
 import express, { Request, Response, Router } from "express";
 
 import locationsRouter from "./locations";
+import resultsTouter from "./results";
 
 dotenv.config();
 
 const app: Router = express.Router();
 
 app.use("/locations", locationsRouter);
+app.use("/results", resultsTouter);
 
 app.get("/", (req: Request, res: Response) => {
     res.send("API router");
