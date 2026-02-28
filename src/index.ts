4294670ed4e6759e1b92d6ed8240fce8ffb47160
nico <90756549+sengokus@users.noreply.github.com>
2026-02-28 10:45:55 +0800

    fix: revert vercel build

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,42 @@
 import "dotenv/config";
-import app from "./app";
+import cors from "cors";
+import express, { Express, Request, Response } from "express";
 
+// Utils
+import { errorHandler } from "./middlewares/exceptions/app-error";
+// Routes
+import adminRouter from "./routes/admin";
+import apiRouter from "./routes/api";
+import locationsRouter from "./routes/api/locations";
+import resultsRouter from "./routes/api/results"
+import webRouter from "./routes/protected";
+import publicRouter from "./routes/public";
+import { logRequestMiddleware } from "./utils/request-utils";
+import { responseLogger } from "./utils/response-utils";
+
+const app: Express = express();
 const port = process.env.PORT || 3000;
+
+// Express's configurations
+app.use(express.json());
+
+app.use(cors());
+app.use(logRequestMiddleware());
+app.use(responseLogger);
+
+app.use("/admin", adminRouter);
+app.use("/api", apiRouter);
+app.use("/public", publicRouter);
+app.use("/web", webRouter);
+
+app.use("/api/locations", locationsRouter);
+app.use("/api/results", resultsRouter);
+
+app.use(errorHandler);
+
+app.get("/", (req: Request, res: Response) => {
+    res.send(`Sticker Map Server\n\nBuild time: ${process.env.BUILD_TIME}`);
+});
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,6 @@
-import { SupabaseClient, createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.DATABASE_URL;
-const supabaseApiKey = process.env.API_KEY;
+const supabaseUrl = process.env.DATABASE_URL!;
+const supabaseApiKey = process.env.API_KEY!;
 
-if (!supabaseUrl || !supabaseApiKey) {
-    throw new Error("Missing DATABASE_URL or API_KEY, set them in the environment variables.");
-}
-
-export const db: SupabaseClient = createClient(supabaseUrl, supabaseApiKey);
+export const db = createClient(supabaseUrl, supabaseApiKey);
